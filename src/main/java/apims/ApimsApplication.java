1e352b9c07258073eed587c1b389620ae21d9c02
yuri neves <yurineves1934@gmail.com>
2023-10-16 18:06:19 -0300

    feat: first commit

---
+++ src/main/java/apims/ApimsApplication.java
@@ -0,0 +1,13 @@
+package apims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApimsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApimsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/apims/ApimsApplicationTests.java
@@ -0,0 +1,13 @@
+package apims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApimsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
