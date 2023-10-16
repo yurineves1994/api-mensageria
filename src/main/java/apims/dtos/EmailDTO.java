7df1f4206d1844e8494ac5fa48efb6ebce6846bc
yuri neves <yurineves1934@gmail.com>
2023-10-16 18:07:00 -0300

    feat: create DTO Email

---
+++ src/main/java/apims/dtos/EmailDTO.java
@@ -0,0 +1,12 @@
+package apims.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record EmailDTO(
+        @NotBlank String ownerRef,
+        @NotBlank @Email String emailFrom,
+        @NotBlank @Email String emailTo,
+        @NotBlank String subject,
+        @NotBlank String text) {
+}
